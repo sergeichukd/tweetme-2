d217849fa911c3aff9a0fd4c80b6a032454ad700
Sergeichuk Dmitrii <sergeychukdmitry@gmail.com>
2020-05-23 19:36:56 +0300

    15 - Tweet List View

--- course_progress.md
+++ course_progress.md
@@ -1,2 +1,4 @@
-10. - Intro to URL Routing and Dynamic Routing
-11. - Handling Dynamic Routing [started]
+10 - Intro to URL Routing and Dynamic Routing
+11 - Handling Dynamic Routing [started]
+15 - Tweet List View
+16 - Dynamic Load Tweets via JavaScript
--- tweets/views.py
+++ tweets/views.py
@@ -10,6 +10,19 @@ def home_view(request, *args, **kwargs):
     # return HttpResponse("<h1> Hello, World! </h1>")
     return render(request, "pages/home.html", context={}, status=200)
 
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Android
+    return json data
+    """
+    qs = Tweet.objects.all()
+    tweets_list  = [{"id": x.id, "content": x.content} for x in qs]
+    data = {
+        "responnse": tweets_list
+    }
+    return JsonResponse(data)
+
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
     """
     REST API VIEW
