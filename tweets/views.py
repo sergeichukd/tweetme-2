05e1138af63e593511e61fa36858e3a84505ce1f
Sergeichuk Dmitrii <sergeychukdmitry@gmail.com>
2020-05-23 19:16:44 +0300

    14 - Bootstrap & Django Templates

--- tweets/views.py
+++ tweets/views.py
@@ -1,15 +1,30 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
+from django.http import JsonResponse
+from django.shortcuts import render
 
 from .models import Tweet
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1> Hello, World! </h1>")
+    # return HttpResponse("<h1> Hello, World! </h1>")
+    return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Android
+    return json data
+    """
+    data = {
+        "id": tweet_id
+    }
     try:
         obj = Tweet.objects.get(id=tweet_id)
+        data["content"] = obj.content
+        status = 200
     except:
-        raise Http404
-    return HttpResponse(f"<h1> Hello, {tweet_id} - {obj.content}! </h1>")
+        data["message"] = "not found"
+        status = 404
+    return JsonResponse(data, status=status)  # json.dumps content_type='application/json'
+
